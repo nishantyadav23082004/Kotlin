cbd978bc35d8a14becaa4a7f8dc5f55f2f491270
nishantyadav23082004 <113028102+nishantyadav23082004@users.noreply.github.com>
2024-09-24 13:26:34 +0530

    Create constraintClass.kt

---
+++ constraintClass.kt
@@ -0,0 +1,20 @@
+package com.example.ca1
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class constraintClass : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_constraint_class)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+}
